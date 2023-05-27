a0ad47e8808d7e01ea1a92c356160331500541db
EvanFarris <evanfarris0@gmail.com>
2023-05-27 16:50:43 -0500

    Implementing solution to pass freeCodeCamp's tests

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require(`dns`);
+let bodyParser = require(`body-parser`);
+let OTSmap = new Map();
+let STOmap = new Map();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -12,6 +16,44 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post(`/api/shorturl`, function(req,res){
+  let originalURL = req.body.url;
+  const regex = /^https?:\/\//i;
+  const regex2 = /\/\?.*$/;
+  //originalURL = originalURL;
+  let hostName = originalURL.replace(regex,``).replace(regex2,``);
+  
+
+  dns.lookup(hostName, function(err,address){
+    if(err){
+
+      res.json({"error": "invalid url"});
+    } else {
+      let shortenedURL = OTSmap.get(originalURL);
+      if(!shortenedURL) {
+        OTSmap.set(originalURL, (OTSmap.size + 1));
+        STOmap.set(OTSmap.size, originalURL);
+        shortenedURL=STOmap.size;
+      }
+
+      res.json({original_url: originalURL, short_url: shortenedURL});
+    }
+  });
+});
+
+app.get(`/api/shorturl/:number`, function(req,res) {
+  let num = parseInt(req.params.number);
+
+  if(num <= STOmap.size){
+
+    res.status(301).redirect(STOmap.get(num));
+  } else {
+    res.json({"error": "No short URL found for the given input"});
+  }
+  
 });
 
 // Your first API endpoint
